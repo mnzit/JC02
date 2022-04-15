4c2f9eb999900240ffafa6483144916dda9e3570
mnzit <mnzitshakya@gmail.com>
2022-04-15 23:33:04 +0545

    Day 02

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,71 @@
 public class Main {
 
-    // Primitive Datatype
-    int i = 1;
-    float j = 1.34f;
-    double k = 1.3;
-    short m = 12;
-    boolean z = true;
-    long x = 100;
-    char nn = 'A';
-
-    // Wrapper Datatype
-    Integer ii = 1;
-    Float jj;
-    String name = "Abiral";
-
     public static void main(String[] args) {
-        int numbers[] = new int[3]; // 0,1
+        /**
+         *   2 * 3
+         *
+         *  5 6 7
+         *  3 4 5
+         */
 
-        for (int i = 0; i < numbers.length; i++) {
-//            System.out.println("Inserting: "+ i);
-//            if( i == 1){
-//                numbers[i] = i;
-//
+
+        /**
+         *   3 * 2
+         * 1 2
+         * 1 2
+         * 1 2
+         */
+
+        int a[][] = {
+                {2, 0},
+                {1, 9}
+        };
+        int b[][] = {
+                {3, 9},
+                {4, 7},
+        };
+
+
+        String[][] friends= {
+                {"","*","","*","*"},
+                {"*","","","","*"},
+                {"","*","","",""},
+                {"*","","","","*"},
+                {"*","*","","*",""}
+        };
+
+
+
+ /*     0+6+0*4=6 0
+        0 0
+
+         */
+
+
+        int z = a[0][1];
+        System.out.println(z);
+
+        int[][] c = new int[a.length][b[0].length];
+//        System.out.println(c.length);
+        for(int row = 0; row < 2; row++){
+            int colLength = c[row].length;
+//            System.out.println(colLength);
+            for(int col = 0; col < 2; col++){
+                int jLength = a[row].length;
+//                System.out.println(jLength);
+                for(int j = 0; j<2; j++) {
+                    System.out.println( c[row][col]+ "+="+ a[row][j] +"*" +b[j][col]);
+                    c[row][col] += a[row][j] * b[j][col];
+                }
+            }
         }
 
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println("numbers[" + i + "]:" + numbers[i]);
+        for(int row = 0; row < c.length; row++){
+            for(int col = 0; col < c[row].length; col++){
+                System.out.print(c[row][col] + " ");
+            }
+            System.out.println();
         }
-
     }
+
 }
